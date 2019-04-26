f46719079cd0e020142ffd5ec233d7bb3ef795eb
DiogoVD <diogovdamascena@gmail.com>
2019-04-26 12:24:48 -0300

    Adicionando os exercicios

--- Curso Java/src/Aula2/ExerciciosAula.java
+++ Curso Java/src/Aula2/ExerciciosAula.java
@@ -7,7 +7,7 @@ public class ExerciciosAula {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Exercicio1();
+		Exercicio3();
 
 	}
 	
@@ -23,6 +23,28 @@ public class ExerciciosAula {
 		System.out.print("i+d="+(int)(i+d));
 		System.out.print("b="+b);
 		if(b==true)System.out.print("positivo");else System.out.print("falso");
+	}
+	
+	public static void Exercicio2(){
+		int l=393;
+		byte b;
+		b = (byte)l;
+		System.out.print("O valor de b é "+b);
+		
+		/* Explicação: O Tipo inteiro possui 4 bytes em sua totalidade de range
+		  como o tipo byte possui apenas 1 byte ele considera apenas o byte menos 
+		  significativo do inteiro. resultando em: 
+		  int 393 = 00000000 00000000 00000001 10001001
+		  ao converter fica assim:
+		  byte 10001001 = -119 */
+	}
+	public static void Exercicio3(){
+		int i;
+		for(i=1;i<=500;i++){
+			if(i%2==0 && i<=498)System.out.print(i+", ");
+			if(i%2==0 && i>498)System.out.print(i+".");
+		}
+		/*Programa para imprimir todos os numeros pares de 1 a 500.*/
 	}
 
 }
