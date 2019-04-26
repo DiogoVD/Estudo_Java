a1e582314e354d84f2b913ebfd0f037f16730e80
DiogoVD <diogovdamascena@gmail.com>
2019-04-26 11:49:22 -0300

    Adicionando aula 2

---
+++ Curso Java/src/Aula2/ExerciciosAula.java
@@ -0,0 +1,28 @@
+package Aula2;
+
+public class ExerciciosAula {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exercicio1();
+
+	}
+	
+	public static void Exercicio1(){
+		int i=10;
+		double d=9.85;
+		char c='a';
+		boolean b=true;
+		
+		System.out.print("i="+i);
+		System.out.print("d="+d);
+		System.out.print("c="+c);
+		System.out.print("i+d="+(int)(i+d));
+		System.out.print("b="+b);
+		if(b==true)System.out.print("positivo");else System.out.print("falso");
+	}
+
+}
