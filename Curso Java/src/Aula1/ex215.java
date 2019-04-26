76a53000823189d94f2e8341ef3f7c81ae9f9b78
DiogoVD <diogovdamascena@gmail.com>
2019-04-26 10:07:29 -0300

    Primeiro commit exercicio atividade 1

---
+++ Curso Java/src/Aula1/ex215.java
@@ -0,0 +1,31 @@
+package Aula1;
+import java.util.*;
+public class ex215 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		quesitoA();
+		System.out.println("");
+		Scanner s = new Scanner(System.in);
+		quesitoB();
+	}
+	
+	public static void quesitoA(){
+		int i=1;
+		for(i=1 ;i <= 4;i++){
+			System.out.println((i-1)+" "+i+" "+(i+1));
+		}
+	}
+	public static void quesitoB(){
+		
+		System.out.println(0+" "+1+" "+2);
+		System.out.println(1+" "+2+" "+3);
+		System.out.println(2+" "+3+" "+4);
+		System.out.println(3+" "+4+" "+5);
+
+}
+
+}
